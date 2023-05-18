d2323959a1477ae0b44a7e3885d15c2a5f7e5fcd
Chevvu <127694928+Chevvu@users.noreply.github.com>
2023-05-18 12:43:45 -0400

    Home work-7

---
+++ HW07/test/edu/umb/cs680/hw07/FileTest.java
@@ -0,0 +1,32 @@
+package edu.umb.cs680.hw07;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileTest {
+    private FileSystem fileSystem;
+    @BeforeEach
+    public void setUp(){
+        fileSystem = TestFixtureInitializer.getFileSystem();
+    }
+    private String[] filetoStrArr(File f){
+        String[] fileinfo = {f.getName(), f.getParent().getName(), String.valueOf(f.getSize())};
+        return fileinfo;
+    }
+    @Test
+    public void FileCTest(){
+        Directory lib = (Directory) fileSystem.getRootsDirs().get(0).getChildren().get(1);
+        File c = lib.getFiles().get(0);
+        assertArrayEquals(new String[]{"c", "lib", "1006"}, filetoStrArr(c));
+    }
+    @Test
+    public void isDirectoryTest(){
+        Directory lib = (Directory) fileSystem.getRootsDirs().get(0).getChildren().get(1);
+        File c = lib.getFiles().get(0);
+        assertFalse(c.isDirectory()
+        );
+    }
+
+}
